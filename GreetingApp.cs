02679c1ebb4efe9b6e860d9177da72f2e5b38668
cedcedie <99469318+cedcedie@users.noreply.github.com>
2024-02-17 21:05:37 +0800

    Add files via upload

---
+++ GreetingApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace GreetingApp
+{
+    class GreetingProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Please enter the number of enrolled courses: ");
+            int numberOfCourses = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter the price of your favorite book: ");
+            double bookPrice = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Number of enrolled courses: " + numberOfCourses);
+            Console.WriteLine("Price of my favorite book: " + bookPrice);
+        }
+    }
+}
